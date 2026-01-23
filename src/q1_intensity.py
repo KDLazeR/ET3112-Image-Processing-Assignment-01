6cb9a36dc030ba2f2db7f9f7b331456bd5421b18
Kanchana Dissanayake <kanchanaprabuddha@gmail.com>
2026-01-23 12:29:11 +0530

    Completed q1 part c

--- src/q1_intensity.py
+++ src/q1_intensity.py
@@ -4,7 +4,8 @@ import matplotlib.pyplot as plt
 
 # load the runway image in grayscale
 img = cv2.imread('images/runway.png', cv2.IMREAD_GRAYSCALE)
-#safety Check
+
+# safety Check
 if img is None:
     print("error: cant find the image file")
 else:
@@ -21,25 +22,57 @@ else:
     table2 = np.array([((i / 255.0) ** gamma2) * 255 for i in range(256)]).astype("uint8")
     res_b = cv2.LUT(img, table2)
 
+    # --- part c: contrast stretching ---
+    # creating a function for the piecewise linear transform
+    def contrast_stretch(image):
+        # convert to float 0-1 range first
+        norm_img = image.astype("float32") / 255.0
+        output = np.zeros_like(norm_img)
+        
+        # thresholds given in the assignment
+        r1 = 0.2
+        r2 = 0.8
+        
+        # 1. pixels between r1 and r2 -> stretch them
+        mask_mid = (norm_img >= r1) & (norm_img <= r2)
+        output[mask_mid] = (norm_img[mask_mid] - r1) / (r2 - r1)
+        
+        # 2. pixels above r2 -> make them white (1.0)
+        mask_high = norm_img > r2
+        output[mask_high] = 1.0
+        
+        # 3. pixels below r1 stay 0 (black), handled by zeros_like init
+        
+        # convert back to 0-255
+        return (output * 255).astype("uint8")
+
+    res_c = contrast_stretch(img)
+
     # show results
-    plt.figure(figsize=(12, 5))
+    plt.figure(figsize=(10, 8))
 
     # og
-    plt.subplot(1, 3, 1)
+    plt.subplot(2, 2, 1)
     plt.imshow(img, cmap='gray')
     plt.title('original')
     plt.axis('off')
 
-    #  gamma 0.5
-    plt.subplot(1, 3, 2)
+    # gamma 0.5
+    plt.subplot(2, 2, 2)
     plt.imshow(res_a, cmap='gray')
     plt.title('gamma = 0.5')
     plt.axis('off')
 
-    #  gamma 2.0
-    plt.subplot(1, 3, 3)
+    # gamma 2.0
+    plt.subplot(2, 2, 3)
     plt.imshow(res_b, cmap='gray')
     plt.title('gamma = 2.0')
+    plt.axis('off')
+    
+    # contrast stretching
+    plt.subplot(2, 2, 4)
+    plt.imshow(res_c, cmap='gray')
+    plt.title('contrast stretching')
     plt.axis('off')
 
     plt.tight_layout()
